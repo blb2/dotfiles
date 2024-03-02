c1a758cb7e663603e35b39f712473a9a960bbca0
Brian Brice <brian@heapify.org>
2024-03-01 23:35:13 -0600

    add tigrc

--- install.bat
+++ install.bat
@@ -6,4 +6,7 @@ copy gvimrc "%USERPROFILE%\_gvimrc" > nul
 if exist "%USERPROFILE%\vimfiles" rd /S /Q "%USERPROFILE%\vimfiles"
 xcopy vim "%USERPROFILE%\vimfiles" /E /I /Y /Q > nul
 
+echo Copying tig configuration
+copy tigrc "%USERPROFILE%\.tigrc" > nul
+
 pause
--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ cp gvimrc ~/.gvimrc
 [ -d ~/.vim ] && rm -r ~/.vim
 cp -a vim ~/.vim
 
+echo Copying tig configuration
+cp tigrc ~/.tigrc
+
 echo Copying tmux configuration ...
 cp -a tmux.conf ~/.tmux.conf
