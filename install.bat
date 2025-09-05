3d235b2631d6beae2f0d0a9f5637fcea33d27a5b
Brian Brice <brian@heapify.org>
2025-09-05 11:16:28 -0500

    copy vim configuration files to more locations
    This is to ensure vim from Git for Windows loads the correct
    configuration files as well.

--- install.bat
+++ install.bat
@@ -5,8 +5,11 @@ if exist "%USERPROFILE%\_vimrc"   del      "%USERPROFILE%\_vimrc"
 if exist "%USERPROFILE%\_gvimrc"  del      "%USERPROFILE%\_gvimrc"
 if exist "%USERPROFILE%\vimfiles" rd /S /Q "%USERPROFILE%\vimfiles"
 
+if not exist "%USERPROFILE%\.config" mkdir "%USERPROFILE%\.config"
+
 echo Copying vim configuration
-xcopy vim "%USERPROFILE%\vimfiles" /E /I /Y /Q > nul
+xcopy vim "%USERPROFILE%\vimfiles"    /E /I /Y /Q > nul
+xcopy vim "%USERPROFILE%\.config\vim" /E /I /Y /Q > nul
 
 echo Copying tig configuration
 copy tigrc "%USERPROFILE%\.tigrc" > nul
