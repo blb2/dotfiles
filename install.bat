8e3c243bb610d0b0d26a182f513f77aed6b0cd2f
Brian Brice <brian@heapify.org>
2026-01-20 16:49:09 -0600

    ensure alternative locations for vim configs are removed before copying

--- install.bat
+++ install.bat
@@ -1,9 +1,10 @@
 @echo off
 
 echo Removing previous configuration
-if exist "%USERPROFILE%\_vimrc"   del      "%USERPROFILE%\_vimrc"
-if exist "%USERPROFILE%\_gvimrc"  del      "%USERPROFILE%\_gvimrc"
-if exist "%USERPROFILE%\vimfiles" rd /S /Q "%USERPROFILE%\vimfiles"
+if exist "%USERPROFILE%\_vimrc"      del      "%USERPROFILE%\_vimrc"
+if exist "%USERPROFILE%\_gvimrc"     del      "%USERPROFILE%\_gvimrc"
+if exist "%USERPROFILE%\vimfiles"    rd /S /Q "%USERPROFILE%\vimfiles"
+if exist "%USERPROFILE%\.config\vim" rd /S /Q "%USERPROFILE%\.config\vim"
 
 if not exist "%USERPROFILE%\.config" mkdir "%USERPROFILE%\.config"
 
